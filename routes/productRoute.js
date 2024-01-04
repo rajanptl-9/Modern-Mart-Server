3f5a242a1a67c2679937116547ea864fe3adc991
Rajan <rajanpatel2684@gmail.com>
2024-01-05 01:13:54 +0530

    Admin authentication to update and delete products with token

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { createProduct, getOneProduct, getAllProducts, updateProduct, deleteProduct } = require('../controller/productCtrl');
+const { authHandler, isAdmin } = require('../middlewares/authHandler');
 
-
-router.post('/create-product',createProduct);
+router.post('/create-product',authHandler,isAdmin,createProduct);
 router.get('/get-a-product/:id',getOneProduct);
 router.get('/get-all-product',getAllProducts);
-router.put('/update-product/:id',updateProduct);
-router.delete('/delete-product/:id',deleteProduct);
+router.put('/update-product/:id',authHandler,isAdmin,updateProduct);
+router.delete('/delete-product/:id',authHandler,isAdmin,deleteProduct);
 
 module.exports = router;
