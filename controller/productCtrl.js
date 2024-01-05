3e3e8305d2590f8becb04cd6f84eaf1e25bf2f45
Rajan <rajanpatel2684@gmail.com>
2024-01-06 02:10:50 +0530

    Querying data (filtering, sorting, pagination)

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -26,7 +26,44 @@ const getOneProduct = asyncHandler(async(req,res) => {
 
 const getAllProducts = asyncHandler(async(req,res) => {
     try {
-        const prod = await Product.find();
+        const queryObj = {...req.query};
+
+        //filtering query
+        const removeFields = ['page','sort','limit','fields'];
+        removeFields.forEach(param => delete queryObj[param]);        
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        let query = Product.find(JSON.parse(queryStr));
+        
+        //sorting query
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(",").join(" ");
+            query = query.sort(sortBy);
+        }else{
+            query = query.sort("-createdAt")
+        }
+
+        //limiting query
+        if(req.query.fields){
+            const fields = req.query.fields.split(",").join(" ").trim();
+            query = query.select(fields);
+        }else{
+            query = query.select("-__v");
+        }
+        
+        //paginating query
+        const page = req.query.page;
+        const limit = req.query.limit || 12;
+        const skip = (page - 1) * limit;
+        query = query.skip(skip).limit(limit);
+        if(req.query.page){
+            const totalProducts = await Product.countDocuments();
+            if(skip >= totalProducts) throw new Error("This Page does not exists");
+        }
+
+        const prod = await query;
         res.json(prod);
     } catch (error) {
         throw new Error(error);
