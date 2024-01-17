2fe828b4807f3db3565f894bcfecbd211be984e3
Rajan <rajanpatel2684@gmail.com>
2024-01-17 17:38:58 +0530

    Order schema and cart schema added

---
+++ models/cartModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+var cartSchema = new mongoose.Schema({
+    products: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Product",
+        },
+        count: Number,
+        color: String,
+        price: Number,
+    }],
+    cartTotal: Number,
+    totalAfterDiscount: Number,
+    orderedBy:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User",
+    }
+},{
+    timestamps: true,
+});
+
+module.exports = mongoose.model('Cart', cartSchema);
